d0018ef86ec62c79686bdedee5ba72a69f4bee2d
Edwin <edwin12sv@gmail.com>
2025-08-24 17:06:47 -0600

    first commit

---
+++ 01-react-foundation/src/App.tsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <main>
+        <h1>Introducción a React</h1>
+        <p>
+          React es una biblioteca de JavaScript para construir interfaces de usuario.
+        </p>
+      </main>
+    </>
+  )
+}
+
+export default App
