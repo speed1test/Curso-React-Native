de45774d5d689fb5582cc68efed9e8a24b07a28b
Edwin <edwin12sv@gmail.com>
2025-09-07 14:57:24 -0600

    Videos de curso

---
+++ 06-MyFirstApp/App.tsx
@@ -0,0 +1,15 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import { HelloWorldScreen } from './src/screens';
+
+function App() {
+  return <HelloWorldScreen />;
+}
+
+export default App;
